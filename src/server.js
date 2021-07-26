0aa2db8896ff41c3ec8cfcc198bcadfad34627da
Pietro Medeiros <spogmedeiros@gmail.com>
2021-07-26 17:21:31 -0300

    add route 'clientes'

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-console.log("YES! Server is running!")
+const express = require('express')
+
+const app = express()
+
+app.use('/api/', routes)
+
+const port = process.env.PORT || 5000
+app.listen(port, ()=> console.log("YES! Server is running!"))
